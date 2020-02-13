fe21581d27fcf2179a85440ef8f89baae7194d27
Sandeep H M <ee15btech11011@iith.ac.in>
2020-02-13 12:42:27 +0530

    dadad

--- BD_test.py
+++ BD_test.py
@@ -13,6 +13,7 @@ import time
 
 files = glob.glob("E:\ECG Matlab\PTBDB_BDFE_GOLDEN\PTDB\*.csv")
 
+'''
 for filepath in files:
 
         list1 = []
@@ -45,19 +46,19 @@ for filepath in files:
         #workbook = xlsxwriter.Workbook(s)
         #worksheet = workbook.add_worksheet()
 
-            
+
 
         for j in range(12):
 
                 boundary_I,R_peak_index_I = Boundary_Detection(Denoised[:,j])
-                
+
                 R_peak_idx = 0
 
                 for i in range(len(R_peak_index_I)):
                         if( (boundary_I[0]<R_peak_index_I[i]) and (boundary_I[1]>R_peak_index_I[i]) ):
                                 R_peak_idx = i
                                 break
-                
+
                 P_array = []
                 QRS_array = []
                 T_array  = []
@@ -73,7 +74,7 @@ for filepath in files:
 
                 print(time.process_time() - start)
         '''
-
+'''
 
         for j in range(12):
                 plt.figure(j+1)
@@ -89,7 +90,7 @@ for filepath in files:
                 plt.plot(X,Denoised[:,j])
                 plt.plot(boundary_I,bd_values,'o')
                 plt.plot(R_peak_index_I,R_values,'o')
-                
+
 
                 row = 0
                 column = j
@@ -103,8 +104,35 @@ for filepath in files:
                 for item in R_peak_index_I:
                         worksheet.write(row,column, item)
                         row += 1
-                
+
 
         #workbook.close()
         plt.show()
-        '''
+'''
+
+list1 = []
+s = ""
+head_tail = os.path.split(filepath)
+tail = head_tail[1]
+tail= tail.split('.csv')
+name = tail[0]
+list1.append('E:\Python Updated Code\BD+FE_python\\')
+list1.append(name)
+list1.append('.xlsx')
+s = s.join(list1)
+print(s)
+
+
+data_f = pd.read_csv(filepath, sep=',',header=None)
+data = data_f.as_matrix()
+
+
+Denoised = np.zeros((len(data),12))
+
+
+for i in range(12):
+        Denoised[:,i] = denoising(data[:,i])
+
+boundary_I,R_peak_index_I = Boundary_Detection(Denoised[:,1])
+
+print(R_peak_index_I) 
