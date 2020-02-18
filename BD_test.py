a2b536aaf17fcac4335810c55229f84918ff6f9f
Sandeep H M <ee15btech11011@iith.ac.in>
2020-02-18 17:18:09 +0530

    Nineth commit. Ran FE for all files under healthy control except four. The results were written to a excel file. Need to compare them with MATLAB output

--- BD_test.py
+++ BD_test.py
@@ -33,7 +33,7 @@ def Feature_extraction(ecg_signal,boundary,R_peak):
 
     return P_vector,QRS_vector,T_vector
 
-files = glob.glob("E:\ECG Matlab\PTBDB_BDFE_GOLDEN\PTDB\*.csv")
+
 
 '''
 for filepath in files:
@@ -134,121 +134,154 @@ for filepath in files:
 
 if __name__ == '__main__':
 
-    filepath = files[0]
-    list1 = []
-    s = ""
-    head_tail = os.path.split(filepath)
-    tail = head_tail[1]
-    tail= tail.split('.csv')
-    name = tail[0]
-    list1.append('E:\Python Updated Code\BD+FE_python\\')
-    list1.append(name)
-    list1.append('.xlsx')
-    s = s.join(list1)
-    print(s)
+    files = glob.glob("E:\ECG Matlab\PTBDB_BDFE_GOLDEN\PTDB\*.csv")
+
+    for filepath in files:
 
 
-    data_f = pd.read_csv(filepath, sep=',',header=None)
-    data = data_f.as_matrix()
+        list1 = []
+        s = ""
+        head_tail = os.path.split(filepath)
+        tail = head_tail[1]
+        tail= tail.split('.csv')
+        name = tail[0]
+        list1.append('E:\Python Updated Code\BD+FE_python\\')
+        list1.append(name)
+        list1.append('.xlsx')
+        s = s.join(list1)
+        print(s)
 
 
-    Denoised = np.zeros((len(data),12))
+        data_f = pd.read_csv(filepath, sep=',',header=None)
+        data = data_f.as_matrix()
 
-    start_time = time.time()
 
-    ######################################## Filtering ECG Signal
+        Denoised = np.zeros((len(data),12))
 
-    ecg_in = []
+        start_time = time.time()
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+        ######################################## Filtering ECG Signal
+
+        ecg_in = []
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for i in range(0,12):
+                ecg_in.append(executor.submit(denoising,data[:,i]))
+
+            i = 0
+            concurrent.futures.wait(ecg_in, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
+            for f in ecg_in:
+                Denoised[:,i] = f.result()
+                i = i + 1
+
+
+        ################################################################
+
+
+        ######################################## Boundary Detection on ECG Signal
+        bd_param = []
+        bd_results = []
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for i in range(0,12):
+                bd_param.append(executor.submit(Boundary_Detection,Denoised[:,i]))
+
+            concurrent.futures.wait(bd_param, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
+            for f in bd_param:
+                bd_results.append(f.result())
+
+        R_peak_idx = 0
+        ################################################################
+
+        fe_param = []
+        fe_results = []
+        '''
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for i in range(0,12):
+                fe_param.append(executor.submit(Feature_extraction,Denoised[:,i],bd_results[i][0],bd_results[i][1]))
+
+            concurrent.futures.wait(fe_param, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
+            for f in fe_param:
+                fe_results.append(f.result())
+        '''
+
         for i in range(0,12):
-            ecg_in.append(executor.submit(denoising,data[:,i]))
+            res = Feature_extraction(Denoised[:,i],bd_results[i][0],bd_results[i][1])
+            fe_results.append(res)
 
-        i = 0
-        concurrent.futures.wait(ecg_in, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
-        for f in ecg_in:
-            Denoised[:,i] = f.result()
-            i = i + 1
+        end_time = time.time()
+        print(end_time-start_time)
 
 
-    ################################################################
+        workbook = xlsxwriter.Workbook(s)
+        worksheet = workbook.add_worksheet()
+
+        for j in range(0,12):
+
+            #plt.figure(j+1)
+
+            X = np.arange(1,10001)
+
+            bd_values = []
+            for i in bd_results[j][0]:
+                    bd_values.append(Denoised[i,j])
+
+            R_values = []
+            for i in bd_results[j][1]:
+                    R_values.append(Denoised[i,j])
 
 
-    ######################################## Boundary Detection on ECG Signal
-    bd_param = []
-    bd_results = []
+            P_wave_idx = []
+            P_wave_values = []
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for i in range(0,12):
-            bd_param.append(executor.submit(Boundary_Detection,Denoised[:,i]))
+            for i in fe_results[j][0]:
+                if(i != -1):
+                    i = int(i)
+                    P_wave_idx.append(i)
+                    P_wave_values.append(Denoised[i,j])
 
-        concurrent.futures.wait(bd_param, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
-        for f in bd_param:
-            bd_results.append(f.result())
+            row = 0
+            column = j
 
-    R_peak_idx = 0
-    ################################################################
+            for item in P_wave_idx:
+                    worksheet.write(row,column, item)
+                    row += 1
+
+            QRS_wave_idx = []
+            QRS_wave_values = []
+            for i in fe_results[j][1]:
+                if(i != -1):
+
+                    i = int(i)
+                    QRS_wave_idx.append(i)
+                    QRS_wave_values.append(Denoised[i,j])
+
+            row = 0
+            column = 12+j
+            for item in QRS_wave_idx:
+                    worksheet.write(row,column, item)
+                    row += 1
+
+            T_wave_idx = []
+            T_wave_values = []
+            for i in fe_results[j][2]:
+                if(i != -1):
+                    i = int(i)
+                    T_wave_idx.append(i)
+                    T_wave_values.append(Denoised[i,j])
+
+            row = 0
+            column = 24+j
+            for item in T_wave_idx:
+                    worksheet.write(row,column, item)
+                    row += 1
 
 
-    ######################################## FE on ECG Signal
-    fe_param = []
-    fe_results = []
+            #plt.plot(X,Denoised[:,j])
+            #plt.plot(bd_results[j][0],bd_values,'o')
+            #plt.plot(bd_results[j][1],R_values,'o')
+            #plt.plot(P_wave_idx,P_wave_values,'*')
+            #plt.plot(QRS_wave_idx,QRS_wave_values,'*')
+            #plt.plot(T_wave_idx,T_wave_values,'*')
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for i in range(0,12):
-            fe_param.append(executor.submit(Feature_extraction,Denoised[:,i],bd_results[i][0],bd_results[i][1]))
-
-        concurrent.futures.wait(fe_param, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
-        for f in fe_param:
-            fe_results.append(f.result())
-    ################################################################
-
-
-    for j in range(0,12):
-
-        plt.figure(j+1)
-
-        X = np.arange(1,10001)
-
-        bd_values = []
-        for i in bd_results[j][0]:
-                bd_values.append(Denoised[i,j])
-
-        R_values = []
-        for i in bd_results[j][1]:
-                R_values.append(Denoised[i,j])
-
-        P_wave_idx = []
-        P_wave_values = []
-
-        for i in fe_results[j][0]:
-            if(i != -1):
-                i = int(i)
-                P_wave_idx.append(i)
-                P_wave_values.append(Denoised[i,j])
-
-        QRS_wave_idx = []
-        QRS_wave_values = []
-        for i in fe_results[j][1]:
-            if(i != -1):
-
-                i = int(i)
-                QRS_wave_idx.append(i)
-                QRS_wave_values.append(Denoised[i,j])
-
-        T_wave_idx = []
-        T_wave_values = []
-        for i in fe_results[j][2]:
-            if(i != -1):
-                i = int(i)
-                T_wave_idx.append(i)
-                T_wave_values.append(Denoised[i,j])
-
-        plt.plot(X,Denoised[:,j])
-        #plt.plot(bd_results[j][0],bd_values,'o')
-        #plt.plot(bd_results[j][1],R_values,'o')
-        plt.plot(P_wave_idx,P_wave_values,'*')
-        plt.plot(QRS_wave_idx,QRS_wave_values,'*')
-        plt.plot(T_wave_idx,T_wave_values,'*')
-
-    plt.show()
+        workbook.close()
